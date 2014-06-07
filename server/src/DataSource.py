a78fbaa80e71cfd63bcc1d392e349486b88b5067
Daniel Piet <dpiet@hubspot.com>
2014-06-07 19:40:15 -0400

    dao complete

--- server/src/DataSource.py
+++ server/src/DataSource.py
@@ -1,4 +1,17 @@
+import pickle
+
 class DataSource(object):
   def __init__(self):
+    self.storage = dict()
     print 'Started datasource'
-  
+
+  def put(self,id,obj):
+    self.storage[id] = obj
+
+  def get(self,id):
+    if id in self.storage:
+      return self.storage[id]
+
+  def save(self):
+    with open("database.pickle", "wb") as f:
+      pickle.dump(self.storage, f)
