3e394cb9c3abe8be6d905b7950b82b5be66237bd
Daniel Piet <dpiet@hubspot.com>
2014-06-07 20:03:18 -0400

    uncomment

--- server/src/JamrResource.py
+++ server/src/JamrResource.py
@@ -1,6 +1,8 @@
 import cherrypy
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
 from ws4py.websocket import EchoWebSocket, WebSocket
+from ws4py.manager import WebSocketManager
+
 
 cherrypy.config.update({'server.socket_port': 9000})
 WebSocketPlugin(cherrypy.engine).subscribe()
@@ -16,11 +18,20 @@ class Root(object):
     handler = cherrypy.request.ws_handler
 
 class Server(WebSocket):
+  def __init__(self):
+    self.init_wsm()
+
+  def init_wsm(self):
+    self.manager = WebSocketManager()
+    self.manager.start()
+
   def opened(self):
+    self.manager.add(self)
     print "Succesfully opened connection"
 
   def received_message(self, message):
     print 'Server echoing: ' + message.data
+    print self.peer_address
     self.send(message.data, message.is_binary)
 
 if __name__ == '__main__':
